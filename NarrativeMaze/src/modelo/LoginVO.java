4dd3ac96d3035a1c5362bea92e496dcd1786560e
caioweiser <114372632+caioweiser@users.noreply.github.com>
2023-10-09 20:27:22 -0300

    Add files via upload

---
+++ NarrativeMaze/src/modelo/LoginVO.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author 182210010
+ */
+public class LoginVO {
+    private String nome;
+    private String senha;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
